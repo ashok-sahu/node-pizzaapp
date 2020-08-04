8f44baeccbd4187b50fe7b02a3230f7bfc9da042
ashok-sahu <ashoksahu1105@gmail.com>
2020-08-04 21:34:56 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const ejs = require("ejs");
+const path = require("path");
+const expressLayout = require("express-ejs-layouts");
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.use(expressLayout);
+app.set("views", path.join(__dirname, path.join("./resources/views")));
+app.set("view engine", "ejs");
+
+app.listen(PORT, () => {
+  console.log(`server is running on http://localhost:${PORT}`);
+});
